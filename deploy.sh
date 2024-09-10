cdaf1d7c280efed76362c6c23c0bfced027bdc3d
hesic73 <hesicheng20@gmail.com>
2024-09-09 23:05:41 -0700

    some update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "Building the React app..."
+
+# Build and start the Next.js frontend
+echo "Building the Next.js frontend..."
 cd frontend && npm run build
+
+echo "Starting the Next.js frontend..."
+npm run start &  # Start the frontend in the background
+
+# Start the FastAPI backend
 echo "Starting the FastAPI app in production mode..."
-cd ../backend && gunicorn --bind 0000:8000 -w 4 -k uvicorn.workers.UvicornWorker app.main:app
+cd ../backend && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.main:app
